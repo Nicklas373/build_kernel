2e10d4ddb8111d52edfc897a7a7e7bf11a2c5a9d
Dicky Herlambang <herlambangdicky5@gmail.com>
2020-05-08 07:11:28 +0700

    ci: trigger: Automated Trigger CI
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -4,6 +4,10 @@
 # Copyright (C) 2020 HANA-CI Build Project (@nicklas373)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+# Cloning Kernel Repository
+# Only enable this if want to compile lavender
+git clone --depth=1 -b fusion-rework https://Nicklas373:$git_token@github.com/Nicklas373/kernel_xiaomi_lavender-4.4
+
 # Clone compiler script & execute it
 wget --output-document=.ci https://raw.githubusercontent.com/Nicklas373/CI/CI/ci
 chmod +x .ci
